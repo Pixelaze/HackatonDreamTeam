cfa9f5bbc16470b01e93d64a331311ff878b5ae4
AndreiKon <kororenko.2007@mail.ru>
2021-07-06 11:56:20 +0300

    Xakaton added

---
+++ Xakaton.py
@@ -0,0 +1,18 @@
+import telebot
+bot = telebot.TeleBot('1834921230:AAFZNRZfuIzBjKES5ITcTwUkGyTqkoWIUWk')
+@bot.message_handler(commands=['Начать'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я - бот. Я слежу за состоянием окружающей среды в классах. Приятно познакомиться, {message.from_user.first_name}. Что вы хотите узнать?')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'Список помещений.':
+        bot.send_message(message.from_user.id, '')
+    elif message.text.lower() == 'Карта этажа [номер этажа]':
+        bot.send_message(message.from_user.id, '')
+    elif message.text.lower() == 'Состояние класса [номер класса]':
+        bot.send_message(message.from_user.id, '')
+    elif message.text.lower() == 'Состояние класса [номер класса]':
+        bot.send_message(message.from_user.id, '')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+bot.polling(none_stop=True)
