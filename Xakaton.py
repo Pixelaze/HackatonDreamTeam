b3fe9879fd056abda6d1bea8bce72117e53780d8
AndreiKon <87003681+AndreiKon@users.noreply.github.com>
2021-07-06 13:24:30 +0300

    Update Xakaton.py

--- Xakaton.py
+++ Xakaton.py
@@ -5,11 +5,11 @@ def send_welcome(message):
     bot.reply_to(message, f'Я - бот. Я слежу за состоянием окружающей среды в классах. Приятно познакомиться, {message.from_user.first_name}. Что вы хотите узнать?')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text.lower() == 'Список помещений.':
+    if message.text.lower() == 'список помещений.':
         bot.send_message(message.from_user.id, '')
-    elif message.text.lower() == 'Карта этажа [номер этажа]':
+    elif message.text.lower() == 'карта этажа [номер этажа]':
         bot.send_message(message.from_user.id, '')
-    elif message.text.lower() == 'Состояние класса [номер класса]':
+    elif message.text.lower() == 'состояние класса [номер класса]':
         bot.send_message(message.from_user.id, '')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
