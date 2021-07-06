c5cde459f84bbb91374d09627b0d33c04b7feb5d
Pixelaze <isvova23@gmail.com>
2021-07-06 13:11:49 +0500

    Первый коммит

---
+++ DataAnalyze.py
@@ -0,0 +1,20 @@
+class DataAnalyzer:
+    def __init__():
+        pass #TODO
+
+    def sendRoomsArray(self, rooms):
+        pass #TODO
+
+class Room:
+    def __init__(self, status):
+        pass #TODO
+
+class Status:
+    BEST = 0
+    GOOD = 1
+    MEDIUM = 2
+    BAD = 3
+    WORST = 4
+
+class Map:
+    pass #TODO
---
+++ Parser.py
@@ -0,0 +1,8 @@
+import requests
+
+class Parser:
+    def __init__(self):
+        pass #TODO
+
+    def giveData(self, response):
+        pass #TODO
